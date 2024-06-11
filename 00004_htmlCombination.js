9bfd1759d3b7d22a766e9ee6b8633e4b8cbcc759
30309-84\3030984 <seaheonseok@gmail.com>
2024-06-11 14:30:36 +0900

    まだXML読み取るコードが解決してない。 修正必要あり。

---
+++ 00004_htmlCombination.js
@@ -0,0 +1,12 @@
+//
+function combination(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading content:', error));
+}
+
+combination('00002_header.html', 'header');
+combination('00003_content.html', 'content');
